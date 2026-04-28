1a6c1f25553c7558c41092264fbfbfb44e7d144f
nbarreto2026 <nestorbarreto2017@gmail.com>
2026-04-28 10:41:39 -0500

    Update swing_strategy.py

--- app/strategies/swing_strategy.py
+++ app/strategies/swing_strategy.py
@@ -1,1 +1,21 @@
+def generate_signal(df):
+    try:
+        ultimo = df.iloc[-1]
 
+        precio = float(ultimo['Close'])
+        ema9 = float(ultimo['EMA_9'])
+        ema26 = float(ultimo['EMA_26'])
+        ema50 = float(ultimo['EMA_50'])
+        rsi = float(ultimo['RSI'])
+
+        if precio > ema50 and ema9 > ema26 and rsi < 65:
+            return {
+                "precio": precio,
+                "rsi": rsi
+            }
+
+        return None
+
+    except Exception as e:
+        print("Error generando señal:", e)
+        return None
