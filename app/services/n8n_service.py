8170b28323f855ae207ee1afaffcb1a25fbec266
nbarreto2026 <nestorbarreto2017@gmail.com>
2026-04-28 10:42:26 -0500

    Update n8n_service.py

--- app/services/n8n_service.py
+++ app/services/n8n_service.py
@@ -1,1 +1,9 @@
+import requests
+from app.config import N8N_WEBHOOK_URL
 
+def enviar_a_n8n(data):
+    try:
+        response = requests.post(N8N_WEBHOOK_URL, json=data)
+        print(f"n8n response: {response.status_code}")
+    except Exception as e:
+        print("Error enviando a n8n:", e)
