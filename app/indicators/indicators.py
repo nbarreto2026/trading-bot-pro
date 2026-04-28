5264acf8186d177efaae242afc3e922fb7c5a0d7
nbarreto2026 <nestorbarreto2017@gmail.com>
2026-04-28 10:41:01 -0500

    Update indicators.py

--- app/indicators/indicators.py
+++ app/indicators/indicators.py
@@ -1,1 +1,9 @@
+import pandas_ta as ta
 
+def apply_indicators(df):
+    df['EMA_9'] = ta.ema(df['Close'], length=9)
+    df['EMA_26'] = ta.ema(df['Close'], length=26)
+    df['EMA_50'] = ta.ema(df['Close'], length=50)
+    df['RSI'] = ta.rsi(df['Close'], length=14)
+
+    return df
